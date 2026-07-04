efd9722b7b339474d20b6cb1ee38530d7c809400
Ari Sandy Kurniawan <arisandyk32@gmail.com>
2026-07-04 20:56:26 +0700

    Materi 1: Variabel & Konstanta & Materi 2: Integer

--- SwiftDasar/main.swift
+++ SwiftDasar/main.swift
@@ -1,11 +1,15 @@
-//
-//  main.swift
-//  SwiftDasar
-//
-//  Created by Ari Sandy Kurniawan on 04/07/26.
-//
-
 import Foundation
 
-print("Hello, World!")
+// MARK: - 01. Variabel dan Konstanta
+// Konstanta tidak dapat diubah nilainya
+// Variabel dapat diubah nilainya
+let namaCourse = "Dicoding"
+var umur = 24
+print("Belajar di", namaCourse)
 
+// MARK: - 02. Tipe Data Integer
+// Lanjut materi kedua di bawahnya
+var skorBermain: Int = 100
+var hargaItem = 30
+skorBermain = skorBermain - hargaItem
+print("Sisa skor:", skorBermain)
