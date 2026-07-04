a86b3e17fa68f765bb0d5ecc4ea72a16145acfd4
Ari Sandy Kurniawan <arisandyk32@gmail.com>
2026-07-04 21:18:34 +0700

    Materi 3: Floatin-Point

--- SwiftDasar/main.swift
+++ SwiftDasar/main.swift
@@ -8,8 +8,24 @@ var umur = 24
 print("Belajar di", namaCourse)
 
 // MARK: - 02. Tipe Data Integer
-// Lanjut materi kedua di bawahnya
+//
 var skorBermain: Int = 100
 var hargaItem = 30
 skorBermain = skorBermain - hargaItem
 print("Sisa skor:", skorBermain)
+
+// MARK: - 03. Floating-Point Number
+// Double (64-bit) 15 digit dibelakang koma
+// Float (32-bit) 6 digit dibelakang koma
+let pajakToko: Float = 0.11
+let beratApel: Double = 2.5
+var hargaSekilo = 15_000.00
+
+var totalHarga = hargaSekilo * beratApel
+var nominalPajak = totalHarga * Double(pajakToko)
+var hargaFinal = totalHarga + nominalPajak
+print("Total Harga Sebelum Pajak: Rp", totalHarga)
+print("Pajak Toko: Rp",nominalPajak)
+print("Total Harga Setelah Pajak: Rp", hargaFinal)
+
+
